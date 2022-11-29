a076687930531cf6b876dfeac0c084dffc1ab098
abdzk <102174098+abdzk@users.noreply.github.com>
2022-11-29 10:19:30 +0100

    modif champ customers-count

--- application/views/backend/users.php
+++ application/views/backend/users.php
@@ -269,14 +269,8 @@
                                             <label for="customer">
                                                 <?= lang('customers_count') ?>
                                                 <span class="text-danger">*</span>
-                                            </label>
-                                            <select id="customers_count" class="form-control required">
-                                                <option value="10">10</option>
-                                                <option value="20">20</option>
-                                                <option value="30">30</option>
-                                                <option value="40">40</option>
-                                                <option value="50">50</option>
-                                            </select>
+                                            <input  type="number" id="customers_count" class="form-control required">
+                                            </input>
                                         </div>
 
 
